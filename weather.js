fd013dcddb14a644ad057e09dd6c3b4439c05282
Vladimir Zhukov <zhukovvlad@yandex.ru>
2023-03-18 21:47:04 +0300

    Added saving token in file

--- weather.js
+++ weather.js
@@ -1,6 +1,16 @@
 #!/usr/bin/env node
 import { getArgs } from "./helpers/args.js";
-import { printHelp } from "./services/log.service.js";
+import { printHelp, printSuccess, printError } from "./services/log.service.js";
+import { saveKeyValue } from "./services/storage.service.js";
+
+const saveToken = async (token) => {
+  try {
+    await saveKeyValue("token", token);
+    printSuccess("Token saved");
+  } catch (error) {
+    printError(error.message);
+  }
+};
 
 const initCLI = () => {
   const args = getArgs(process.argv);
@@ -8,10 +18,10 @@ const initCLI = () => {
     printHelp();
   }
   if (args.s) {
-	// Save City
+    // Save City
   }
   if (args.t) {
-	// Save token
+    return saveToken(args.t);
   }
   // Return Weather
 };
